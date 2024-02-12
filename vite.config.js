400c477aba4492650cc7cdcc5f6c7680eb3c19ec
janily <nanhuchenjie@163.com>
2024-02-12 22:36:01 +0800

    feat: og图片配置文件调整

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,26 @@
 import { defineConfig } from 'vite';
+import ogPlugin from 'vite-plugin-open-graph'
+
+const ogOptions = {
+  basic: {
+    url: 'https://backgroundears-erase.xyz',
+    title: 'Backgroundears erase',
+    type: 'image.png',
+    image: 'https://backgroundears-erase.xyz/og.png',
+    determiner: 'auto',
+    description: 'Remove background in seconds',
+    locale: 'es_ES',
+    localeAlternate: ['es_ES'],
+    siteName: 'Backgroundears erase',
+  },
+  twitter: {
+    image: 'https://backgroundears-erase.xyz/og.png',
+    imageAlt: 'twitter image alt',
+  },
+};
+
 export default defineConfig({
+  plugins: [ogPlugin(ogOptions)],
   build: {
     target: 'esnext'
   }
